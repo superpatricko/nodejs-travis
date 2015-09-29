49fe7d079a12aea181cf004dc8028aae231a260e
Patrick Nguyen <superpatricko@gmail.com>
2015-09-29 15:26:24 -0400

    Add travis.yml, Makefile and other stuff
    - Add travis.yml
    - Add Makefile
    - Modify gulpfile to trim out the 'gulp mocha' commands that shouldn't
      be used. We should be using the 'make test' command
    - Add the 'make test' command to the the package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 "use strict";
 
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var gutil = require('gulp-util');
 var nodemon = require('gulp-nodemon');
 
 gulp.task('default', function() {
@@ -17,15 +15,4 @@ gulp.task('default', function() {
 	.on('restart', function() {
 		console.log('Restarting...');
 	});
-});
-
-gulp.task('mocha', function() {
-	return gulp.src(['test/*.js'], {read: false})
-		.pipe(mocha({reporter: 'list'}))
-		.on('error', gutil.log);
-});
-
-gulp.task('watch-mocha', function() {
-	gulp.run('mocha');
-	gulp.watch(['./**/*.js', 'test/**/*.js'], ['mocha']);
 });
